fd56945e1aff8f28e800ee54565daae89d2a2ef1
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-10-23 15:38:47 -0400

    Setup of Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,57 @@
-function App() {
-  return <h1> Hello from App </h1>;
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import * as sessionActions from "./actions/session";
+
+import './index.css';
+import Splash from "./components/Splash";
+
+function Layout() {
+  const dispatch = useDispatch();
+
+  const sessionUser = useSelector(state => state.session.user);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser());
+  }, [dispatch]);
+
+  return (
+    <div className="app-container">
+      {
+        sessionUser ? (
+          <>
+            <header className="app-header">
+              <h1>Welcome to the App!</h1>
+            </header>
+            <main className="app-main">
+              <Outlet />
+            </main>
+            <footer className="app-footer">
+              <span>This is footer</span>
+            </footer>
+          </>
+        ) : (
+          <main className="app-main">
+            <Outlet />
+          </main>
+        )
+      }
+    </div>
+  )
 }
 
-export default App;
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Splash />
+      }
+    ]
+  }
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />
+}
---
+++ frontend/src/components/Splash/Splash.jsx
@@ -0,0 +1,9 @@
+import './Splash.css';
+
+export default function Splash() {
+    return (
+        <div className="splash">
+            <h1>Welcome to the Splash Page!</h1>
+        </div>
+    );
+}
